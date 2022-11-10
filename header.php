5aacb28a7f9ad73d8207cac343ea8a4966e946b3
Eugene Kabeyi <eugenekabeyi144@gmail.com>
2022-11-10 11:19:33 +0300

    new update

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet " href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/jquery.min.js "></script>
+    <script src="bootstrap/js/bootstrap.min.js "></script>
+
+    </head>
+    <body>
+        
+        <footer></footer>
+        
+ 
