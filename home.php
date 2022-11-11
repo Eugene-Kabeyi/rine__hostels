31eaabdcf2e7a5fddb9f2a6c78a32125f34d005e
Eugene Kabeyi <eugenekabeyi144@gmail.com>
2022-11-11 07:17:54 +0300

    update included four new files

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+include "header.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rine Hostels</title>
+    </head>
+    <body>
+        <div class="col-md-9 col-lg-9 bg-light">
+            
+        </div>
+
+    </body>
+</html>
