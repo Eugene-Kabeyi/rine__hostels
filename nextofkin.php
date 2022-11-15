856c572a9b917ca3b924c5ccb885b2e1768e7a47
Eugene Kabeyi <eugenekabeyi144@gmail.com>
2022-11-15 13:19:45 +0300

    org php created and next of kin form made

--- nextofkin.php
+++ nextofkin.php
@@ -19,11 +19,51 @@ include "header.php"?>
 </head>
 
 
+</head>
 
 <div class="col-md-9 col-lg-9 bg-light">
+    <!--Applicants booking-->
+    <br>
+    <h3 class="text-info">Please fill in the details below :</h3>
+    <br>
+    <!--payer details-->
+    <h5 class="text-primary">Kin Details</h5>
+    <div class="row m-2 p-2">
+        <div class="col">
+            <label class="form-label grey">First Name:</label>
+            <input type="text" class="form-control" placeholder="First name" aria-label="First name" name="fname_app"
+                required>
+
+        </div>
+        <div class="col">
+            <label class="form-label grey">Last Name:</label>
+            <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" name="lname_app"
+                required>
+
+        </div>
+
+    </div>
+    <div class="row m-2 p-2">
+        <div class=" col mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address:</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                name="email_app" placeholder="name@example.com" required>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+    </div>
+    <div class="row m-2 p-2">
+        <div class="col-md-6">
+            <label class="form-label grey">Phone Number</label>
+            <input class="form-control" type="text" name="phoneNumber" required>
+
+        </div>
+        <div>
+            <hr>
+        </div>
+
     
 
-</div>
+
 </main>
 
 <?php
